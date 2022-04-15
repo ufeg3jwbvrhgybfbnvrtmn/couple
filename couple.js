8274994615b718dd5259e1dd8efd316314a18477
ufeg3jwbvrhgybfbnvrtmn <103780257+ufeg3jwbvrhgybfbnvrtmn@users.noreply.github.com>
2022-04-15 14:52:29 -0500

    Add files via upload

---
+++ couple.js
@@ -0,0 +1,41 @@
+document.write("<p><marquee>");
+var day = new Date();
+var mo = date.getMonth();
+
+if (mo == 0) {
+ document.write("Long last Kadrian");
+}
+if (mo == 1) {
+ document.write("Long last Adriami");
+}
+if (mo == 2) {
+ document.write("Long last Komian");
+}
+if (mo == 3) {
+ document.write("Long last Adriomi");
+}
+if (mo == 4) {
+ document.write("Long last Adriai");
+}
+if (mo == 5) {
+ document.write("Long last Korian");
+}
+if (mo == 6)  {
+ document.write("Long last Komdrian");
+}
+if (mo == 7) {
+ document.write("Long last Krian");
+}
+if (mo == 8) {
+ document.write("Long last Adri");
+}
+if (mo == 9) {
+ document.write("Long last Koan");
+}
+if (mo == 10)  {
+ document.write("Long last Aomi");
+}
+if (mo === 11) {
+ document.write("Long last Adromi");
+}
+document.write("</p></marquee>");
